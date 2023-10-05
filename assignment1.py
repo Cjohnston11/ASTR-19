13984ddf33b9788cb02947c894c673da04cc5a3f
Cjohnston11 <146883255+Cjohnston11@users.noreply.github.com>
2023-10-05 12:21:20 -0700

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,11 +1,14 @@
+#this defines the printassignment funtion as a list of print functions
 def PrintAssignment1 ():
 	print("Hello! I am Cari Johnston (she her)")
 	print("My favorite movie is currently interstellar")
 	print("My favorite food right now is sushi")
-	
+
+#this defines the main function
 def main ():
 	PrintAssignment1 ()
 	
+#this tells the program what to execute
 if __name__=="__main__":
 	main()
 
