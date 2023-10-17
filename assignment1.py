f22e0445d51ea393ddbe7e9d04f475105bdd8a0d
Cjohnston11 <146883255+Cjohnston11@users.noreply.github.com>
2023-10-17 12:39:24 -0700

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,6 @@
 #this defines the printassignment funtion as a list of print functions
 def PrintAssignment1 ():
-	print("Hello! I am Cari Johnston (she her)")
+	print("Hello! I am Cari Johnston (she/her)")
 	print("My favorite movie is currently interstellar")
 	print("My favorite food right now is sushi")
 
