fc529fb833f19c20e16e15ff45597321dd1e4275
Cjohnston11 <146883255+Cjohnston11@users.noreply.github.com>
2023-10-17 12:45:08 -0700

    Add

---
+++ assignment2.py
@@ -0,0 +1,18 @@
+x = 19.24 #define two floating point numbers
+y = 78.1
+
+print ("x =", x, (type(x))) #print the variables and their type 
+print ("y =", y, (type(y)))
+print("x+y=",x+y, (type(x+y))) #add the floating point numbers and their type
+
+a=20 #define two integers
+b=8
+print ("a =", a, (type(a))) #print the variables and their type
+print ("b =", b, (type(b)))
+print("a-b=",a-b, (type(a-b))) #subtract the integers and print their type
+
+w = 11.98 #define a floating point number
+z = 13 #define a integer
+print ("w =", w, (type(w))) #print the variables and their type
+print ("z =", z, (type(z)))
+print ("w*z =", w*z, (type(w*z))) #multiply the floating point number and the integer together adn then print the type of the answer
